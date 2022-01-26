f18880b9c4c5ca9b95d12097f2316c3d5192476d
pandarenr <pandarenr@gmail.com>
2022-01-26 19:30:33 +1000

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,8 +16,31 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        DB::table('users')->insert(
+            [   'name' => 'test',
+                'email' => 'test@test.test',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ]
+        );
+        DB::table('users')->insert(
+            [   'name' => 'test2',
+                'email' => 'test2@test.test',
+                'email_verified_at' => now(),
+                'password' => Hash::make('password'),
+                'remember_token' => Str::random(10),
+            ]
+        );
+        $data = [
+            ['name'=>'Физическая культура и спорт'],
+            ['name'=>'Педагог профессионального обучения'],
+            ['name'=>'ИЗО и МХК'],
+            ['name'=>'Информационные технологии'],
+        ];
+        DB::table('subjects')->insert($data);
         \App\Models\User::factory(10)->create();
-        \App\Models\Subject::factory(4)->create();
-        \App\Models\Course::factory(20)->create();
+        \App\Models\Course::factory(15)->create();
+
     }
 }
