a765b0422393e1ff638e037aa379dfeb2d1edb2e
pandarenr <pandarenr@gmail.com>
2023-01-25 18:53:17 +1000

    edit auth page

--- resources/views/components/auth-card.blade.php
+++ resources/views/components/auth-card.blade.php
@@ -1,5 +1,5 @@
-<div class="flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-    <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
+<div class="flex sm:justify-center items-center">
+    <div class="w-full py-4 sm:max-w-md bg-white overflow-hidden">
         {{ $slot }}
     </div>
 </div>
